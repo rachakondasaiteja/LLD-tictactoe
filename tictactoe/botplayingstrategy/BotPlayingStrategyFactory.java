857d6a6454dfc652dfe4cbd4b4877fc502fe5f6f
Saiteja R <rachakondasteja@gmaail.com>
2024-02-24 17:00:58 +0530

    Last commit

---
+++ tictactoe/botplayingstrategy/BotPlayingStrategyFactory.java
@@ -0,0 +1,11 @@
+package tictactoe.botplayingstrategy;
+
+import tictactoe.models.BotDifficultyLevel;
+
+public class BotPlayingStrategyFactory {
+
+    public static BotPlayingStrategy getBotPlayingStrategyForDifficultyLevel(BotDifficultyLevel botDifficultyLevel) {
+        // have if else and implement on your own
+        return new EasyBotPlayingStrategy();
+    }
+}
